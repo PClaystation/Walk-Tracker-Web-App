77a5f150d219c50e711b73187acfbf24cf8271d5
PClaystation <152532670+PClaystation@users.noreply.github.com>
2025-03-06 19:38:58 +0100

    :(

--- script.js
+++ script.js
@@ -606,3 +606,16 @@ window.addEventListener('beforeinstallprompt', (event) => {
     });
   });
 });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then((registration) => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch((error) => {
+          console.log('Service Worker registration failed:', error);
+        });
+    });
+  }
+  
